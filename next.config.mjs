a59ac9e901a8f013d34e052100e12698ed03ef74
Rahul Badugu <work01220106@gmail.com>
2026-08-11 15:49:33 +0530

    upfated

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import { imageHosts } from './image-hosts.config.mjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: true,
-  output: 'standalone',
+  // output: 'standalone',
   distDir: process.env.DIST_DIR || '.next',
   devIndicators: {
     appIsrStatus: false,
